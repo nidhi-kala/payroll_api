644798edbf6f06154dc6ca97f61fc5fb4057c07a
Nidhi Kala <nidhikala95@gmail.com>
2024-05-28 20:11:09 -0400

    extracting id from the filename

---
+++ app/controllers/time_reports_controller.rb
@@ -0,0 +1,15 @@
+class TimeReportsController < ApplicationController
+
+  def upload
+    file = params[:file]
+    
+    if file.blank?
+      render json: { error: 'No file uploaded'}, status: :bad_request
+      return
+    end
+
+    filename = file.original_filename
+    id = filename.match(/-(\d+)\./).captures.first.to_i
+    binding.pry
+  end
+end
