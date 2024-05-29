e87de04a7c42f8bbe154cb947aba8c6642eec0bd
Nidhi Kala <nidhikala95@gmail.com>
2024-05-29 14:27:32 -0400

    model validations

--- app/models/time_report.rb
+++ app/models/time_report.rb
@@ -1,2 +1,7 @@
 class TimeReport < ApplicationRecord
+  validates :date, presence: true
+  validates :hours_worked, presence: true, numericality: { greater_than 0 }
+  validates :employee_id, presence: true, numericality: { only_integer: true }
+  validates :job_group, presence: true, inclusion: { in: %w(A B) }
+  validates report_id, presence: true, uniqueness: true
 end
