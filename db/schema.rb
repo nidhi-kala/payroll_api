04a976b02e49fac990a86525ede9e5c4b5c065f1
Nidhi Kala <nidhikala95@gmail.com>
2024-05-29 22:45:06 -0400

    drop groups table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_181320) do
-  create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 2024_05_30_024403) do
   create_table "time_reports", force: :cascade do |t|
     t.date "date"
     t.decimal "hours_worked"
